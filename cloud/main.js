6d9fd2bdcb013f4454789c4992e0245b8a03323a
claytonknittel <c.j.knittel@wustl.edu>
2020-08-02 22:29:53 -0500

    create card batch test

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,16 @@
 Parse.Cloud.define('hello', function(req, res) {
   return 'Hi';
 });
+
+Parse.Cloud.define('create_card_batch', function(req, res) {
+  return [
+    {
+      question: "What is my dog's name?",
+      answer: "Remy"
+    },
+    {
+      question: "What is my favorite song?",
+      answer: "..."
+    }
+  ];
+});
