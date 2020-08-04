ee4a3cc2e9747150de714691abe50b08ebeb6573
claytonknittel <c.j.knittel@wustl.edu>
2020-08-03 23:08:02 -0500

    test

--- cloud/main.js
+++ cloud/main.js
@@ -11,15 +11,17 @@ Parse.Cloud.define('create_card_batch', async function(req, res) {
   console.log(userObjs);
 
   const card1 = userObjs[0].get("cards")[0];
+  const q = card1.get("question");
+  const a = card1.get("answer");
 
   console.log(card1);
-  console.log(card1.get("question"));
-  console.log(card1.get("answer"));
+  console.log(q);
+  console.log(a);
 
   return [
     {
-      question: card1.get("question"),
-      answer: card1.get("answer")
+      question: q,
+      answer: a
     }
   ];
 });
