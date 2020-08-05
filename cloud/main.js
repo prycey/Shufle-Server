09cab7cb221f89fc066776fad2711a67fbd5af59
claytonknittel <c.j.knittel@wustl.edu>
2020-08-04 23:02:45 -0500

    choose random 5 users to take cards from

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,5 @@
+const { User } = require("parse");
+
 Parse.Cloud.define('hello', function(req, res) {
   return 'Hi';
 });
@@ -6,10 +8,23 @@ Parse.Cloud.define('hello', function(req, res) {
 Parse.Cloud.define('create_card_batch', async function(req, res) {
   const user = req.user;
 
+  const UserClass = Parse.Object.extend("User");
+  const userQuery = Parse.Query(UserClass);
+  userQuery.notEqualTo("objectId", user.get("objectId"));
+  userQuery.limit(5);
+  const randomUsers = await userQuery.find({ useMasterKey: true });
+
+  // query for these users' cards
   const CardClass = Parse.Object.extend("Card");
-  const query = new Parse.Query(CardClass);
-  query.notEqualTo("owner", user);
-  const cards = await query.find({ useMasterKey: true });
+  let queryList = [];
+  randomUsers.forEach((user) => {
+    const query = new Parse.Query(CardClass);
+    query.equalTo("owner", user);
+    queryList.push(query);
+  });
+
+  const cardQuery = Parse.Query.or(...queryList);
+  const cards = await cardQuery.find({ useMasterKey: true });
 
   console.log(cards);
 
