29ed7110f9a428427ef384b1324a868bbd8d5d65
claytonknittel <c.j.knittel@wustl.edu>
2020-08-24 18:00:45 -0500

    card saving

--- cloud/cards.js
+++ cloud/cards.js
@@ -1,6 +1,37 @@
 fs = require('fs');
 
 const MAX_CARDS = 10;
+const NUM_QUESTIONS = 5;
+
+Parse.Cloud.beforeSave("Card", (request) => {
+    const user = request.user;
+    const card = request.object;
+
+    let q = card.get("question");
+
+    if (q === undefined) {
+        throw "Card must have a question";
+    }
+    if (!Number.isInteger(q)) {
+        throw "Question invalid";
+    }
+    if (q < 0 || q >= NUM_QUESTIONS) {
+        throw "Question invalid";
+    }
+
+    // query for this user's cards
+    const CardClass = Parse.Object.extend("Card");
+    const query = new Parse.Query(CardClass);
+    query.equalTo("owner", user);
+
+    const cards = await cardQuery.find({ useMasterKey: true });
+    cards.forEach((c) => {
+        if (q === c.get("question")) {
+            c.destroy();
+        }
+    });
+
+});
 
 Parse.Cloud.define('get_questions', async function(req, res) {
     let contents = fs.readFileSync('public/assets/data/questions.json', 'utf8');
