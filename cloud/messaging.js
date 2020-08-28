d71b6282ce29b61c6e46630e8ab40f7b86d4d473
claytonknittel <c.j.knittel@wustl.edu>
2020-08-27 22:39:55 -0500

    test

--- cloud/messaging.js
+++ cloud/messaging.js
@@ -3,7 +3,8 @@
  *
  * Conversation database structure:
  * {
- *      users: [user1_ptr, user2_ptr],
+ *      user1: user1_ptr,
+ *      user2: user2_ptr,
  *      timesamp: <last time a message was sent>
  * }
  * 
@@ -32,11 +33,47 @@ Parse.Cloud.define('send_message', async function(req, res) {
  *
  * return format:
  * {
- *      
+ *      user_name: "...",
+ *      last_message: "...",
+ *      timestamp: <time_of_last_message>
  * }
  */
 Parse.Cloud.define('get_conversations', async function(req, res) {
+    const user = req.user;
 
+    const ConvoClass = Parse.Object.extend("Conversation");
+    const query1 = Parse.Query(ConvoClass);
+    const query2 = Parse.Query(ConvoClass);
+    query1.equalTo("user1", user);
+    query2.equalTo("user2", user);
+    const query = Parse.Query.or(query1, query2);
+
+    const convos = await query.find({ useMasterKey: true });
+
+    let convo_list = Promise.all(convos.map(async (convo) => {
+        let otherUserPtr;
+        if (convo.get("user1").equals(user)) {
+            otherUserPtr = convo.get("user2");
+        }
+        else {
+            otherUserPtr = convo.get("user1");
+        }
+
+        const otherQuery = Parse.Query(Parse.User);
+        otherQuery.equalTo("objectId", otherUserPtr.id);
+        let other = await otherQuery.find({ useMasterKey: true });
+        let otherName = other.get("username");
+
+        lastMessageAt = convo.get("timestamp");
+
+        return {
+            user_name: otherName,
+            last_message: "not yet implemented",
+            timestamp: lastMessageAt
+        };
+    }));
+
+    return convo_list;
 });
 
 
@@ -50,3 +87,14 @@ Parse.Cloud.define('get_messages', async function(req, res) {
 
 });
 
+
+Parse.Cloud.define('create_random_convo', async function(req, res) {
+    const ConvoClass = Parse.Object.extend("Conversation");
+
+    let convo = new ConvoClass();
+    convo.set("user1", "PbY2FyGu1g");
+    convo.set("user2", "c7loOCLvrj");
+    convo.set("timestamp", new Date());
+
+    convo.save();
+});
